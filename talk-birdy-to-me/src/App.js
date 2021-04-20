4e31cae15afda3d0458370807391a3c8528df940
Allison <76595505+allisonIsCoding@users.noreply.github.com>
2021-04-20 17:45:56 -0400

    working on handle submit for form submission based on color

--- talk-birdy-to-me/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div>Talk Birdy to Me!</div>;
-}
-export default App;
---
+++ talk-birdy-to-me/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+// import Birds from '../Components/birdData';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    //this should render the birds in the database that are blue;
+    if (this.state.value === "null") {
+      alert('Please pick a color!')
+    }
+    else {
+      alert('A bird color was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  }
+
+  render() {
+    return (
+      < div >
+        <div className="title">
+          <h1>Talk Birdy to Me!</h1>
+          </div>
+         <div className="birdColorForm">
+        <form onSubmit={this.handleSubmit}>
+          <label>What color is the bird? 
+            <select className="form-box" value={this.state.value} onChange={this.handleChange}>
+                <option value="null">Pick a color</option>
+                <option value="blue">Blue</option>
+                <option value="red">Red</option>
+                <option value="brown">Brown</option>
+                <option value="grey">Grey</option>
+                <option value="black">Black</option>
+            </select>    
+          </label>
+          <input type="submit" value="submit" />
+          </form>
+          </div>
+        </div >
+    );
+  }
+}
+export default App;
