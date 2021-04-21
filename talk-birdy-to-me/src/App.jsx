8d0efca7e23c17b1f58ced713c5577d874006526
Allison <76595505+allisonIsCoding@users.noreply.github.com>
2021-04-21 16:43:28 -0400

    inside useEffect

--- talk-birdy-to-me/src/App.jsx
+++ talk-birdy-to-me/src/App.jsx
@@ -1,66 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFeatherAlt } from '@fortawesome/free-solid-svg-icons';
+import createPalette from '@material-ui/core/styles/createPalette';
 // import birdfunc from '../birdFunc';
 // import Birds from '../Components/birdData';
 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+function App()  {
+  const [color, setColor] = useState('no color yet')
+
+  useEffect(() => {
+    // alert('this happened')
+    console.log('rendered', color)
+    
+    //add in conditions to check if setColor is udpated and render accordingly
+    if (color !== 'no color yet') {
+      console.log(`inside ${color} conditional`)
+    }
+
+    // if (color === 'red') {
+    //   console.log('inside red conditional');
+    // }
+    // if (color === 'blue') {
+    //   console.log('inside blue conditional')
+    // }
+
+  })
+    
+  const handleChange = value => {
+    setColor(value);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value})
-  }
-
-  handleSubmit(event) {
-    //this should render the birds in the database that are blue;
-    if (this.state.value === 'null' || this.state.value === '') {
-      alert('Please pick a color!')
-    }
-    else {
-      event.preventDefault();
-      console.log('A bird color was submitted: ' + this.state.value);
-      // birdFunc();
-    }
-  }
   
-   
 
-  render() {
     return (
-
       < div className="container">
         <div className="title">
           <h1>Talk Birdy to Me!</h1>
-          </div>
-         <div className="birdColorForm">
-        <form onSubmit={this.handleSubmit}>
-          <label>What color is the bird? 
-            <select className="form-box" value={this.state.value} onChange={this.handleChange}>
-                <option value="null">Pick a color</option>
-                <option value="blue">Blue</option>
-                <option value="red">Red</option>
-                <option value="brown">Brown</option>
-                <option value="grey">Grey</option>
-                <option value="black">Black</option>
-            </select>    
-          </label>
-          <input type="submit" value="submit" />
-          </form>
-          </div>
+        </div>
+        <div className="birdColorQuestion">What color is the bird?</div>
+        <div className="button-div">
+          <button className='blue-button' onClick={() => { handleChange('blue') }} >Blue Button</button>
+          <button className='red-button' onClick={() => { handleChange('red') }}>Red Button</button>
+        </div>
         </div >
 
     );
-  }
 }
+
+// const handleChange = event => {
+
+//   }
+
+// const handleSubmit = event => {
+//     //this should render the birds in the database that are blue;
+//     if (color === '') {
+//       alert('Please pick a color!')
+//     }
+//     else {
+//       event.preventDefault();
+//       console.log('A bird color was submitted: ' + color);
+//       // birdFunc();
+//     }
+//   }
+  
 export default App;
 
 
